b3432aa0396d24dc6fd5ee2eb898a6c764b568bc
wuhuaping2006@126.com <wuhuaping2006@126.com>
2022-01-13 21:42:45 +0800

    banner

--- spring-boot-mytest/src/main/java/com/wuhp/SpringBootMyTestApplication.java
+++ spring-boot-mytest/src/main/java/com/wuhp/SpringBootMyTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication//springboot的主配置类
-public class SpringBootMytestApplication {
+public class SpringBootMyTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMytestApplication.class, args);
+		SpringApplication.run(SpringBootMyTestApplication.class, args);
 	}
 
 }
